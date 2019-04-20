48eee177a6d31dc757a058f51b4e89ca6a2dd98a
naonaorange <nao.lunalovegood@gmail.com>
2019-04-20 16:09:26 +0900

    add hello world wpf

---
+++ 01_HelloWorldWPF/WpfSample/Bootstrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WpfSample.Views;
+
+using Unity;
+using Prism.Unity;
+using System.Windows;
+
+
+
+namespace WpfSample
+{
+    class Bootstrapper :UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ 01_HelloWorldWPF/WpfSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Prism.Mvvm;
+
+namespace WpfSample.ViewModels
+{
+    class MainWindowViewModel: BindableBase
+    {
+        private string text;
+        public string Text
+        {
+            get { return text; }
+            set { SetProperty(ref text, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            Text = "Hello World WPF";
+        }
+    }
+}
